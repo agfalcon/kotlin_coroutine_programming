c9ab364e4424fa2ad9c83b14f85b47f9bb7a85f1
agfalcon <agfalcon12@gmail.com>
2023-07-08 11:27:44 +0900

    Unconfined

--- src/main/kotlin/Dispatchers.kt
+++ src/main/kotlin/Dispatchers.kt
@@ -1,6 +1,8 @@
 import kotlinx.coroutines.*
 
-fun main() = runBlocking<Unit> {
+fun main() = unConfinedDispatcherFunc()
+
+fun dispatchersFunc() = runBlocking<Unit> {
     launch{
         println("context of parent -> ${Thread.currentThread().name}")
     }
@@ -25,3 +27,13 @@ fun main() = runBlocking<Unit> {
 //IO는 코어 수 보다 훨씬 많은 스레드를 가지는 스레드 풀이다. IO 작업은 CPU를 덜 소모하기 떄문이다.
 //Unconfined는 어디에도 속하지 않는다. 지금 시점에는 부모의 스레드에서 수행될 것이다.
 //newSingleThreadContext는 항상 새로운 스레드를 만든다.
+
+
+fun unConfinedDispatcherFunc() = runBlocking<Unit> {
+    async(Dispatchers.Unconfined){
+        repeat(30){
+            println("Unconfined -> ${Thread.currentThread().name}")
+            delay(100L)
+        }
+    }
+}//Unconfined는 중단점 이후에 어느 dispatcher에서 수행될지 에측하기 어렵다. 가능하면 확실한 dispatcher을 사용하는 것이 좋다.
